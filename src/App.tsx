111bd05f430b82cfe0ee0c6e0e0ca0501115e109
gadkas <mugolnikov@icloud.com>
2024-07-22 22:06:37 +0300

    +cat +Exel

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import max from './assets/max.jpg'
+import logo from './assets/logo.png'
 import InputParameters from './components/InputParameters/InputParameters'
 import { skipperModels } from './constants/skipper'
+import XLSX from 'xlsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [count1, setCount1] = useState(0)
   const [sigmaB, setSigmaB] = useState("")
   const [sigmaF, setSigmaF] = useState("")
   const [bsigma, setBsigma] = useState("")
@@ -20,15 +20,35 @@ function App() {
 
   const [select, setSelect] = useState("modelMarin")
 
+  const handleChange = (e) => {
+    e.preventDefault()
+    var files = e.target.files, f = files[0]
+    var reader = new FileReader()
+    reader.onload = function (e) {
+      if (e.target) {
+        var data = e.target.result
+        let readedData = XLSX.read(data, { type: 'binary' })
+        const wsname = readedData.SheetNames[0]
+        const ws = readedData.Sheets[wsname]
+        const dataParse = XLSX.utils.sheet_to_json(ws, { header: 1 })
+        setFileUploaded(dataParse)
+      }
+    }
+    reader.readAsArrayBuffer(f)
+  }
+
 
   return (
     <>
       <div>
         <a href="https://vk.com/idjetmax" target="_blank">
-          <img src={max} className="logo react max" alt="React logo" />
+          <img src={max} className="max" alt="logo" />
         </a>
       </div>
-      <h1>Программа</h1>
+      <div className='titleContainer'>
+        <h1 className='title'>Fatigue life calculation</h1>
+        <img src={logo} className="logo" alt="logo" />
+      </div>
       <select name="select" value={select} onChange={(e) => setSelect(e.target.value)}>
         <option value="modelMarin" selected>Модель Марина</option>
         <option value="modelCrossland">Модель Кроссланда</option>
@@ -49,12 +69,7 @@ function App() {
         {skipperModels.get(select)?.mytau && <InputParameters value={mytau} setValue={setMytau} title='mytau' />}
       </div>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          {count}
-        </button>
-        <button onClick={() => setCount1((count1) => count1 + 1)}>
-          {count1}
-        </button>
+        <input type="file" onChange={handleChange} />
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
